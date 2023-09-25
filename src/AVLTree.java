1f0a9fa95db3919392566143bd7513d1c56334df
Nathan Wagner <nwkaue1@gmail.com>
2023-09-25 19:09:21 -0300

    node and readme
    add node and preparation for create tree

---
+++ src/AVLTree.java
@@ -0,0 +1,20 @@
+public class AVLTree {
+    private Node root;
+    private int numElements;
+
+    public AVLTree() {
+        this.numElements = 0;
+    }
+
+    public boolean search() {
+        return false;
+    }
+
+    public void insert() {
+
+    }
+
+    public void delete() {
+
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,53 @@
+public class Node {
+    private int root, height;
+    private Node left, right;
+
+    public Node(int root) {
+        this.root = root;
+        left = null;
+        right = null;
+        height = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "root=" + root +
+                ", height=" + height +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    public int getRoot() {
+        return root;
+    }
+
+    public void setRoot(int root) {
+        this.root = root;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
