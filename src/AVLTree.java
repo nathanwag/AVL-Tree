e2f0b5ed1707b0ae7bea3a31a8396587768891e5
Nathan Wagner <nwkaue1@gmail.com>
2023-09-26 17:51:15 -0300

    search, delete and insert
    methods for avltree

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -1,20 +1,89 @@
 public class AVLTree {
     private Node root;
-    private int numElements;
 
     public AVLTree() {
-        this.numElements = 0;
-    }
-
-    public boolean search() {
-        return false;
-    }
-
-    public void insert() {
 
     }
 
-    public void delete() {
+    // SEARCH AN ELEMENT
 
+    public Node search(int key) {
+        return searchRecursive(key, root);
+    }
+
+    private Node searchRecursive(int key, Node node) {
+        if(node == null) {
+            return null;
+        }
+        if(key == node.getRoot()) {
+            return node;
+        } else if(key < node.getRoot()) {
+            return searchRecursive(key, node.getLeft());
+        } else {
+            return searchRecursive(key, node.getRight());
+        }
+    }
+
+    // INSERT AN ELEMENT
+
+    public void insert(int key) {
+        root = insertRecursive(key, root);
+    }
+
+    private Node insertRecursive(int key, Node node) {
+        if(node == null) {
+            node = new Node(key);
+        } else if(key < node.getRoot()) {
+            node.setLeft(insertRecursive(key, node.getLeft()));
+        } else if(key > node.getRoot()) {
+            node.setRight(insertRecursive(key, node.getRight()));
+        } else {
+            System.out.println("There is already an element with this key!");
+        }
+
+        return node;
+    }
+
+    // DELETE AN ELEMENT
+
+    public void delete(int key) {
+        root = deleteRecursive(key, root);
+    }
+
+    private Node deleteRecursive(int key, Node node) {
+        if(node == null) {
+            return null;
+        }
+
+        if(key < node.getRoot()) {
+            node.setLeft(deleteRecursive(key, node.getLeft()));
+        } else if(key > node.getRoot()) {
+            node.setRight(deleteRecursive(key, node.getRight()));
+        }
+
+        // NO CHILDREN
+        else if(node.getLeft() == null && node.getRight() == null) {
+            node = null;
+        }
+        // ONE CHILDREN
+        else if(node.getLeft() == null) {
+            node.setRoot(node.getRight().getRoot());
+        } else if(node.getRight() == null) {
+            node.setRoot(node.getLeft().getRoot());
+        }
+        // TWO CHILDREN
+        else {
+            Node minimumRight = findMinimum(node.getRight());
+            node.setRoot(minimumRight.getRoot());
+            node.setRight(deleteRecursive(minimumRight.getRoot(), node.getRight()));
+        }
+        return node;
+    }
+
+    private Node findMinimum(Node node) {
+        while(node.getLeft() != null) {
+            node = node.getLeft();
+        }
+        return node;
     }
 }
